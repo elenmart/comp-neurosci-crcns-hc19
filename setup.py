3408e54c25596aa61b73496e451c7df1793a93dd
Christof Fehrman <ckf5fh@virginia.edu>
2019-04-05 14:40:56 -0400

    Thanks Dan, for the help

--- setup.py
+++ setup.py
@@ -9,17 +9,22 @@ if sys.hexversion < 0x02070000:
 
 # Replace all instances of `comp-neurosci-skeleton` with the name of your project
 setup(
-    name="comp-neurosci-skeleton",
+    name="comp-neurosci-crcns-hc19",
     version="0.0.1",
-    package_dir={'comp-neurosci-skeleton': 'src'},
-    packages=["comp-neurosci-skeleton"],
+    package_dir={'comp-neurosci-crcns-hc19': 'src'},
+    packages=["comp-neurosci-crcns-hc19"],
 
     description="",
     long_description="",
     install_requires=[
-        "numpy>=1.10",
+        "numpy==1.16.0",
+        "scipy==1.2.0",
+        "pandas==0.24.1",
+        "matplotlib==2.2.2",
+        "seaborn==0.9.0",
+        "notebook==5.7.6",
     ],
 
-    author="Your Name Here",
-    maintainer='Your Name Here',
+    author="Christof Fehrman",
+    maintainer='Christof Fehrman',
 )
